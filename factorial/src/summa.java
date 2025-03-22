d237c018645ae5bfcd615223c63f01670d9f5612
.email <sanjaisanjai6551@gmail.com>
2025-03-22 22:50:20 +0530

    added vanakam

--- factorial/src/summa.java
+++ factorial/src/summa.java
@@ -2,5 +2,6 @@ public class summa {
     public static void main(String[] args) {
         System.out.println("haiiiii");
         System.out.println("sanjai");
+        System.out.println("vanakam");
     }
 }
